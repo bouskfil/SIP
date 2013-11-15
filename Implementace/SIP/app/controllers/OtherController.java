8109198675eff1451c6ebd69efb0c7d505eaccd0
honsdomi <honsdomi@fel.cvut.cz>
2013-11-15 12:23:17 +0100

    Ostatní-přidat uživatele zaklad

--- Implementace/SIP/app/controllers/OtherController.java
+++ Implementace/SIP/app/controllers/OtherController.java
@@ -6,22 +6,31 @@
 
 package controllers;
 
-import play.*;
-import play.mvc.*;
+import models.User;
 import play.data.*;
-import static play.data.Form.*;
-import play.data.validation.Constraints.*;
-import models.*;
-
-
+import play.mvc.*;
 import views.html.other.*;
+import static play.data.Form.*;
 /**
  *
  * @author Dominik
  */
 public class OtherController extends Controller{
+    
+    final static Form<User> userForm = form(User.class, User.All.class);
 
     public static Result choose() {
         return ok(choose.render());
     }
+    
+    public static Result create() {
+        return ok(create.render(userForm));
+    }
+    
+    public static Result add() {
+        User user = Form.form(User.class).bindFromRequest().get();
+        user.save();
+        return redirect(routes.OtherController.choose());
+    }
+    
 }
--- Implementace/SIP/app/models/User.java
+++ Implementace/SIP/app/models/User.java
@@ -11,9 +11,20 @@ import play.db.ebean.Model;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import play.data.validation.Constraints.Required;
+import play.db.ebean.Model;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 public class User extends Model {
+    
+    public interface All{}
 
     @Id
     public String email;
@@ -29,6 +40,10 @@ public class User extends Model {
     public static Finder<String,User> find = new Finder<String,User>(
             String.class, User.class
     );
+    
+    public static void create(User user){
+        user.save();
+    }
 
     public static User authenticate(String email, String password) {
         return find.where().eq("email", email)
