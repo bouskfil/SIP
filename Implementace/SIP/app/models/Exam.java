7a13f787b36690ca0ea490ecdc13a82cf27fb50c
Filip Bouška <bouskfil@fel.cvut.cz>
2013-11-16 20:05:11 +0100

    přidán model Exam

---
+++ Implementace/SIP/app/models/Exam.java
@@ -0,0 +1,13 @@
+package models;
+
+import play.db.ebean.Model;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: MacBook
+ * Date: 16.11.13
+ * Time: 20:04
+ * To change this template use File | Settings | File Templates.
+ */
+public class Exam extends Model {
+}
